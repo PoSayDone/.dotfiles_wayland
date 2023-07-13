738f829d3a6811a7f1b39f61144f4c7ab63c85a7
PoSayDone <poseaydone@ya.ru>
2023-07-13 23:41:32 +0500

    Some changes in install script

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+
 #
 # SCRIPT NOT WORKING NOW, WOULD FIX IN THE FUTURE
 #
+
 DIR=$HOME/.config
 bold=$(tput bold)
 normal=$(tput sgr0)
 
+cd $HOME
 clear
 
 #Dependencies
@@ -15,19 +18,20 @@ yay -Syu --noconfirm 'hyprland-git' 'rofi-lbonn-wayland-only-git' 'alsa-tools' \
   'brightnessctl' 'git' 'zathura' 'zathura-pdf-mupdf' 'bpytop' 'wl-gammactl' \
   'neovim' 'spotify' 'npm' 'lutris' 'discord-screenaudio' 'cmake' \
   'base-devel' 'python' 'python-pip' 'lf' 'joshuto-git' 'meson' \
-  'zsh' 'playerctl' 'pamixer' 'pavucontrol' --needed
+  'zsh' 'playerctl' 'pamixer' 'pavucontrol' 'adw-gtk3' --needed
 
+#Installing ags
 git clone https://github.com/Aylur/ags.git
 cd ags
 meson setup build
 meson install -C build
-
-
-cd $HOME
+clear
 
 #Cloning dots
 echo -e "\e[1;31m${bold}Cloning dotfiles${normal}\e[0m"
 git clone https://github.com/PoSayDone/.dotfiles_wayland.git
+
+#Removing previous configs
 rm -rf\
   $DIR/hypr\
   $DIR/ags\
@@ -36,16 +40,18 @@ rm -rf\
   $DIR/rofi\
   $DIR/lf\
   $DIR/discord-screenaudio\
-  $CONFIG_DIR/gtk-3.0\
+  "$DIR/gtk-3.0"\
+  "$DIR/gtk-4.0"\
 clear
 
 #Symlinks
 echo -e "\e[1;31m${bold}Symlinking dotfiles${normal}\e[0m"
 ln -sf $HOME/.dotfiles_wayland/files/* $DIR
+ln -sf "$DIR/files/gtk-4.0/gtk.css $HOME/.config/gtk-3.0"
 clear
 
 #Fonts
-echo -e "\e[1;31m${bold}Coping fonts${normal}\e[0m"
+echo -e "\e[1;31m${bold}Copying fonts${normal}\e[0m"
 mkdir $HOME/.fonts
 ln -sf $HOME/.dotfiles_wayland/files/additional/fonts/* $HOME/.fonts
 fc-cache -r
@@ -61,7 +67,12 @@ clear
 
 #Astronvim installation
 echo -e "\e[1;31m${bold}Installing Astronvim${normal}\e[0m"
+
 mv ~/.config/nvim ~/.config/nvim.bak
+mv ~/.local/share/nvim ~/.local/share/nvim.bak
+mv ~/.local/state/nvim ~/.local/state/nvim.bak
+mv ~/.cache/nvim ~/.cache/nvim.bak
+
 git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
 mkdir $HOME/.config/nvim/lua/user
 git clone https://github.com/posaydone/astroconf.git $HOME/.config/nvim/lua/user
